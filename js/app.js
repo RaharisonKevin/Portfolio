9ff8b18716bce7aad2772b64c816b355b7f7dbe9
Raharison Kevin <rkevinandy@yahoo.fr>
2022-05-11 03:20:13 +0200

    Redesign of the portfolio
    Refonte graphique totale du portfolio.

---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded");
+    let btn = document.querySelector(".burger");
+    let el = document.querySelector("header div");
+    let close1 = document.querySelector(".close");
+
+    let plus = document.querySelector(".plus1");
+    let el1 = document.querySelector("main div.close-menu");
+
+    btn.addEventListener("click", () => {
+        el.classList.toggle("open-menu");
+        btn.innerHTML === "☰" ? btn.innerHTML = "✕" : btn.innerHTML = "☰";
+    });
+    close1.addEventListener("click", () => {
+        el.classList.toggle("open-menu");
+        btn.innerHTML === "☰" ? btn.innerHTML = "✕" : btn.innerHTML = "☰";
+    });
+});
